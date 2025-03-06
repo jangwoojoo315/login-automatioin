90748cc3c8c726e9da1b07def32f310dc39c8277
WooJuJang <wooju.jang@vatech.com>
2025-03-06 15:14:59 +0900

    계정추가

--- popup.js
+++ popup.js
@@ -7,42 +7,135 @@ document.getElementById("add").addEventListener("click", () => {
   }
 });
 
-document.getElementById("vn").addEventListener("click", () => {
-  const data = ["vn첫 번째 항목", "vn두 번째 항목", "vn세 번째 항목"];
+document.getElementById("clear-all").addEventListener("click", () => {
+  chrome.storage.sync.clear();
+});
+document.getElementById("check-bucket").addEventListener("click", () => {
+  chrome.storage.sync.getBytesInUse(null, (bytes) => {
+    alert(`현재 사용 중인 용량: ${bytes} 바이트`);
+  });
+});
+
+document.getElementById("save").addEventListener("click", async () => {
+  const selectedRadio = document.querySelector('input[name="option"]:checked');
+  const allElement = document.querySelectorAll("#add-login-info input");
+  const [clinicId, userId, password] = Array.from(allElement).map(
+    (input) => input.value
+  );
+
+  let region = "";
+  let domain = "";
+
+  switch (selectedRadio.id) {
+    case "add-vnqa":
+      region = "vn";
+      domain = "vnm2.vnclever.com";
+      break;
+    case "add-egqa":
+      region = "eg";
+      domain = "egy2.vnclever.com";
+      break;
+    case "add-inqa":
+      region = "in";
+      domain = "ind2.vnclever.com";
+      break;
+  }
+
+  chrome.storage.sync.get([region], (result) => {
+    const existingArr = result[region] || [];
+    existingArr.push({
+      clinicId,
+      userId,
+      password,
+    });
+
+    chrome.storage.sync.set({ [region]: existingArr }, () => {
+      console.log("업데이트된 배열 : ", existingArr);
+    });
+  });
+
+  // chrome.runtime.sendMessage({
+  //   action: "add",
+  //   domain: domain,
+  //   clinicIdDB: clinicId,
+  //   userIdDB: userId,
+  //   passwordDB: password,
+  // });
+});
+
+document.getElementById("vn").addEventListener("click", async () => {
+  const result = await chrome.storage.sync.get(["vn"]);
 
   const list = document.getElementById("list");
   list.innerHTML = "";
 
   list.appendChild(document.createElement("div"));
   // data 배열을 순회하면서 span 요소를 생성하고 list에 추가합니다.
-  data.forEach((item) => {
-    const span = document.createElement("p"); // 새로운 span 요소 생성
-    span.textContent = item; // span 요소에 텍스트 값 설정
-    list.appendChild(span); // list div에 span 요소를 추가
+  result["vn"].forEach((item) => {
+    const { clinicId, userId, password } = item;
+    const button = document.createElement("button"); // 새로운 button 요소 생성
+    button.textContent = `${clinicId} / ${userId} / ${password}`;
+
+    button.addEventListener("click", () => {
+      chrome.runtime.sendMessage({
+        action: "start",
+        domain: "vnm2.vnclever.com",
+        clinicIdDB: clinicId,
+        userIdDB: userId,
+        passwordDB: password,
+      });
+    });
+    list.appendChild(button);
   });
 });
-document.getElementById("eg").addEventListener("click", () => {
-  const data = ["eg첫 번째 항목", "eg두 번째 항목", "eg세 번째 항목"];
+document.getElementById("eg").addEventListener("click", async () => {
+  const result = await chrome.storage.sync.get(["eg"]);
 
   const list = document.getElementById("list");
   list.innerHTML = "";
+
+  list.appendChild(document.createElement("div"));
   // data 배열을 순회하면서 span 요소를 생성하고 list에 추가합니다.
-  data.forEach((item) => {
-    const span = document.createElement("p"); // 새로운 span 요소 생성
-    span.textContent = item; // span 요소에 텍스트 값 설정
-    list.appendChild(span); // list div에 span 요소를 추가
+  result["eg"].forEach((item) => {
+    const { clinicId, userId, password } = item;
+    const button = document.createElement("button"); // 새로운 button 요소 생성
+    button.textContent = `${clinicId} / ${userId} / ${password}`;
+
+    button.addEventListener("click", () => {
+      chrome.runtime.sendMessage({
+        action: "start",
+        domain: "egy2.vnclever.com",
+        clinicIdDB: clinicId,
+        userIdDB: userId,
+        passwordDB: password,
+      });
+    });
+    list.appendChild(button);
   });
 });
-document.getElementById("in").addEventListener("click", () => {
-  const data = ["in첫 번째 항목", "in두 번째 항목", "in세 번째 항목"];
+document.getElementById("in").addEventListener("click", async () => {
+  const result = await chrome.storage.sync.get(["in"]);
 
   const list = document.getElementById("list");
   list.innerHTML = "";
+
+  list.appendChild(document.createElement("div"));
   // data 배열을 순회하면서 span 요소를 생성하고 list에 추가합니다.
-  data.forEach((item) => {
-    const span = document.createElement("p"); // 새로운 span 요소 생성
-    span.textContent = item; // span 요소에 텍스트 값 설정
-    list.appendChild(span); // list div에 span 요소를 추가
+  result["in"].forEach((item) => {
+    const { clinicId, userId, password } = item;
+    const button = document.createElement("button"); // 새로운 button 요소 생성
+    button.textContent = `${clinicId} / ${userId} / ${password}`;
+
+    button.addEventListener("click", () => {
+      chrome.runtime.sendMessage({
+        action: "start",
+        domain: "ind2.vnclever.com",
+        clinicIdDB: clinicId,
+        userIdDB: userId,
+        passwordDB: password,
+      });
+    });
+    list.appendChild(button);
   });
 });
 
@@ -55,7 +148,7 @@ document.getElementById("openDashboardTab").addEventListener("click", () => {
 
   chrome.runtime.sendMessage({
     action: "startSessionEnd",
-    url: "",
+    domain: "",
     clinicIdDB: clinicId,
     userIdDB: userId,
     passwordDB: password,
